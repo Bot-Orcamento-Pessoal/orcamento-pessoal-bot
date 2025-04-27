60c0eb59e3cdacf9929111b3051593804e8d0031
Bot-Orcamento-Pessoal <juninhochumbinho@gmail.com>
2025-04-27 10:20:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.get('/', (req, res) => {
     res.send('Bot Orcamento Pessoal está rodando!');
 });
 
+// Rota para receber mensagens do Gupshup
+app.post('/webhook', (req, res) => {
+    const incomingMessage = req.body;
+
+    console.log('Mensagem recebida:', incomingMessage);
+
+    // Aqui você pode responder ou processar a mensagem recebida
+    res.sendStatus(200); // Responde para o Gupshup que recebeu
+});
+
 // Ouvindo na porta que o Render indicar
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
